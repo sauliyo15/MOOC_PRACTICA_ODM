06163b9b3b28eced0aa1fdef75d7f39a396692b0
sauliyo15 <gc.saul.7@gmail.com>
2023-12-07 13:28:30 +0100

    list y read con manejo de errores - OK

--- controllers/patient.js
+++ controllers/patient.js
@@ -1,44 +1,56 @@
 // Cargamos los modelos para usarlos posteriormente
-const Patient = require('../models/patient');
+const Patient = require("../models/patient");
 
-exports.list = async function() {
+exports.list = async function () {
+  try {
     const patients = await Patient.find();
     return patients;
-}
+  } catch (error) {
+    console.error("Error leyendo los pacientes:", error);
+    throw error;
+  }
+};
 
-exports.read = async function(patientId) {
-    const patient = await Patient.findOne({_id: patientId});
+exports.read = async function (patientId) {
+  try {
+    const patient = await Patient.findOne({ _id: patientId });
     return patient;
-}
+  } catch (error) {
+    console.error("Error al buscar el paciente:", error);
+    throw error;
+  }
+};
 
-exports.create = async function(body) {
-    // Rellene aqui ...
-    console.log("BODY: ", body);
+exports.create = async function (body) {
+    
+};
 
-}
+exports.update = async function (patientId, body) {
+  // Rellene aqui ...
+  console.log("BODY: ", body);
+  console.log("patientId: ", patientId);
+};
 
-exports.update= async function(patientId, body) {
-    // Rellene aqui ...
-    console.log("BODY: ", body);
-    console.log("patientId: ", patientId);
-}
-
-exports.delete = async function(patientId) {
-    // Rellene aqui ...
-}
+exports.delete = async function (patientId) {
+  // Rellene aqui ...
+};
 
 exports.filterPatientsByCity = async function (city) {
-    // Rellene aqui ...
-}
+  // Rellene aqui ...
+};
 
 exports.filterPatientsByDiagnosis = async function (diagnosis) {
-    // Rellene aqui ...
-}
+  // Rellene aqui ...
+};
 
-exports.filterPatientsBySpeacialistAndDate = async function (specialist, sDate,fDate) {
-    // Rellene aqui ...
-}
+exports.filterPatientsBySpeacialistAndDate = async function (
+  specialist,
+  sDate,
+  fDate
+) {
+  // Rellene aqui ...
+};
 
 exports.addPatientHistory = async function (patientId, medicalRecord) {
-    // Rellene aqui ...
-}
+  // Rellene aqui ...
+};
